7628782e202a08be71640e639e748b36b5f16527
zhupengfei <2655534232@qq.com>
2023-05-09 19:54:35 +0800

    0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,34 @@
   <div class="box">
     <Header></Header>
     <Time class="time"></Time>
+    <el-button size="large" circle class="counterBtn" @click="counterTag = !counterTag">
+      <svg class="icon cumIcon" aria-hidden="true">
+        <use xlink:href="#icon-jisuanqi"></use>
+      </svg>
+    </el-button>
+
+    <transition enter-active-class="animate__animated animate__fadeInRightBig" leave-active-class="animate__animated animate__fadeOut">
+      <div class="counter" v-if="counterTag">
+        
+      </div>
+    </transition>
+      
   </div>
 </template>
 
 <script setup lang="ts">
+  import "animate.css"
   import Header from './header.vue'
   import Time from './tools-com/time.vue'
+  import { ElMessageBox } from 'element-plus'
   import { ref } from 'vue'
+  import { hook } from '../assets/ts/hook'
 
-  defineProps<{ msg: string }>()
-
-  const count = ref(0)
+  const counterTag = ref(false)
 </script>
 
 <style scoped="true" lang="scss">
+
 .box {
   width: 100%;
   height: 100vh;
@@ -28,11 +42,51 @@
   top: 0;
   right: 1%;
 }
-// 媒体查询
-// @media screen and (max-width:1800px) {
-//     .box {
-//         width: 1920px;  
-//         height: 937px;  
-//     }
-// }
+
+.cumIcon{
+  font-size: 27px;
+}
+.counterBtn{
+  position: fixed;
+  bottom: 0;
+  right: -50px;
+  animation: initCounterBtn 2s ease-in-out;
+  animation-fill-mode: forwards;
+}
+.counter{
+  position: fixed;
+  bottom: 4rem;
+  right: 0;
+  width: 15%;
+  height: 30%;
+  border-radius: 1.5rem;
+  background-color: #89abca;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+.counter::after{
+  content: "";
+  display: inline-block;
+  position: absolute;
+  bottom: -10px;
+  transform: translateX(-50%) rotate(-45deg);
+  background: linear-gradient(45deg, #89abca, #89abca 50%, transparent 50%, transparent 100%);
+  width: 20px;
+  height: 20px;
+  background-color: #89abca;
+}
+
+.el-button--large{
+  width: 50px !important;
+  height: 50px !important;
+}
+@keyframes initCounterBtn{
+  100% {  
+    right: 7.5%;
+    transform: rotate(0deg) translateX(50%);
+  }
+  0% { 
+    right: -50px;
+    transform: rotate(400deg);
+  }
+}
 </style>
