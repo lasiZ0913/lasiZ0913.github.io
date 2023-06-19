99229a08fd1bcf175ecf2e66f60f16b9223f39f5
本机 <2655534232@qq.com>
2023-06-19 23:09:18 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,8 +4,10 @@
     <!-- <Time class="time"></Time> -->
 
     <!-- 临时测试内容 start -->
-    <test @on-click="onClick"></test>
-    
+    <Button name="父组件isShow" @click="isShow = !isShow"></Button>
+    <test v-model="isShow" v-model:text="textVModel"></test>
+    <div>父组件：{{ isShow }} -- {{ textVModel }}</div>
+
     <!-- 临时测试内容 end -->
 
     <!-- <el-button size="large" circle class="counterBtn" @click="counterTag = !counterTag">
@@ -14,9 +16,7 @@
       </svg>
     </el-button> -->
 
-    <transition 
-      :duration="{enter:500, leave:500}"
-      enter-active-class="animate__animated animate__bounceInRight" 
+    <transition :duration="{ enter: 500, leave: 500 }" enter-active-class="animate__animated animate__bounceInRight"
       leave-active-class="animate__animated animate__backOutDown">
       <div class="counter" v-if="counterTag">
         <input type="number" v-model="line">
@@ -27,51 +27,54 @@
         </div>
       </div>
     </transition>
-   
+
   </div>
 </template>
 
 <script setup lang="ts">
-  import _ from 'lodash'
-  import "animate.css"
-  import gsap from 'gsap'
-  // import Header from './header.vue'
-  // import Time from './tools-com/time.vue'
-  import Button from './control-com/button.vue'
-  import test from './control-com/test.vue'
-  import { ElMessageBox } from 'element-plus'
-  import { ref, watch, reactive, shallowRef, triggerRef, shallowReactive, provide } from 'vue'
-  import { hook } from '../assets/ts/hook'
+import _ from 'lodash'
+import "animate.css"
+// import gsap from 'gsap'
+// import Header from './header.vue'
+// import Time from './tools-com/time.vue'
+import Button from './control-com/button.vue'
+import test from './control-com/test.vue'
+import { ElMessageBox } from 'element-plus'
+import { ref, watch, reactive, shallowRef, triggerRef, shallowReactive, provide } from 'vue'
+import { hook } from '../assets/ts/hook'
 
-  // 计算器
-  const counterTag = ref(false)
-  let cost = ref(null)
-  let line = ref(null)
-  let counterResult = ref(0)
-  watch([cost, line], ([costNew, lineNew], [costOld, lineOld]) => {
-    counterResult.value = parseInt(((lineNew - costNew) / costNew * 100).toFixed(2))
-  })
-  const copyCounterResult = () => {
-    line.value = null
-    hook.copy(counterResult, '%')
-  }
+// 计算器
+const counterTag = ref(false)
+let cost = ref(null)
+let line = ref(null)
+let counterResult = ref(0)
+watch([cost, line], ([costNew, lineNew], [costOld, lineOld]) => {
+  counterResult.value = parseInt(((lineNew - costNew) / costNew * 100).toFixed(2))
+})
+const copyCounterResult = () => {
+  line.value = null
+  hook.copy(counterResult, '%')
+}
 
-  // 临时测试
-  const onClick = (e) => {
-    console.log(e);
-  }
-  let flag = ref<number>(1)
-  provide('flag', flag)
-  
+// 临时测试
+const onClick = (e) => {
+  console.log(e);
+}
+let flag = ref<number>(1)
+provide('flag', flag)
+const isShow = ref<boolean>(false)
+const textVModel = ref<string>("dilei")
+
 </script>
 
 <style scoped="true" lang="scss">
 // 测试内容
-.testClass{
+.testClass {
   display: flex;
   flex-wrap: wrap;
   width: calc(35px * 14);
-  .testItem{
+
+  .testItem {
     width: 35px;
     height: 35px;
     border: 1px solid #ccc;
@@ -80,9 +83,11 @@
     border-radius: 50%;
   }
 }
-.mmm{
+
+.mmm {
   transition: all 1s;
 }
+
 .box {
   width: 100%;
   height: 100vh;
@@ -90,66 +95,73 @@
   // background: url("https://s1.ax1x.com/2023/05/03/p9JHKD1.jpg") no-repeat;
   background-size: 100%;
 }
-.time{
+
+.time {
   position: fixed;
   top: 0;
   right: 1%;
 }
 
-.cumIcon{
+.cumIcon {
   font-size: 27px;
 }
-.counterBtn{
+
+.counterBtn {
   position: fixed;
   bottom: 0;
   right: -50px;
   animation: initCounterBtn 2s ease-in-out;
   animation-fill-mode: forwards;
 }
-.counter{
+
+.counter {
   position: fixed;
   bottom: 4rem;
   right: 0;
   width: 15%;
   height: 25%;
   border-radius: 1.5rem;
-  background-color: rgba(255,255,255,.2);
+  background-color: rgba(255, 255, 255, .2);
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   padding-top: 1%;
-  .result{
+
+  .result {
     display: flex;
     justify-content: space-around;
     align-items: center;
-    span{
+
+    span {
       font-size: 1.5rem;
     }
   }
 }
-.counter::after{
+
+.counter::after {
   content: "";
   display: inline-block;
   position: absolute;
   bottom: -10px;
   left: 50%;
   transform: translateX(-50%) rotate(-45deg);
-  background: linear-gradient(45deg, rgba(255,255,255,.2), rgba(255,255,255,.2) 50%, transparent 50%, transparent 100%);
+  background: linear-gradient(45deg, rgba(255, 255, 255, .2), rgba(255, 255, 255, .2) 50%, transparent 50%, transparent 100%);
   width: 20px;
   height: 20px;
-  background-color: rgba(255,255,255,0);
+  background-color: rgba(255, 255, 255, 0);
 }
 
-.el-button--large{
+.el-button--large {
   width: 50px !important;
   height: 50px !important;
 }
-@keyframes initCounterBtn{
-  100% {  
+
+@keyframes initCounterBtn {
+  100% {
     right: 7.5%;
     transform: rotate(0deg) translateX(50%);
   }
-  0% { 
+
+  0% {
     right: -50px;
     transform: rotate(400deg);
   }
-}
-</style>
+}</style>
--- src/components/control-com/test.vue
+++ src/components/control-com/test.vue
@@ -1,18 +1,34 @@
 <template>
-  <div style="background-color: green;">
-      我是B
-      <button @click="change">change falg</button>
-      <div>{{ flag }}</div>
+  <div>
+      我是子组件
+      <Button name="click" @click="change"></Button>
+      <div v-if="modelValue">子组件在此</div>
+      <input type="0000" v-model="text" @input="inputChange">
+      <!-- <div>{{ flag }}</div> -->
+
   </div>
 </template>
-  
+  ``
 <script setup lang='ts'>
+import Button from './button.vue'
 import { inject, Ref, ref } from 'vue'
 
-const flag = inject<Ref<number>>('flag')
+defineProps<{
+  modelValue:boolean,
+  text:string
+}>()
+
+const emit = defineEmits<{
+  (e:'update:modelValue', name:boolean) : void,
+  (e:'update:text', name:string) : void
+}>()
+
 const change = () => {
-  console.log(flag);
-  flag.value = 2
+  emit('update:modelValue', false)
+  emit('update:text', "我明天 还要 大赚！！！")
+}
+const inputChange = (e:Event) => {
+  emit('update:text', (e.target as HTMLInputElement).value)
 }
 </script>
   
